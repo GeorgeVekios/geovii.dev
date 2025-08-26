d92e7a6c720b5b8b1b812531d9ed3576f25126be
George Vekios <georgevekios@gmail.com>
2025-08-26 17:24:48 -0400

    added motion to resume button

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,6 +6,7 @@ import SkillsGrid from "@/components/SkillsGrid";
 import { site } from "@/data/site";
 import { about } from "@/data/about";
 import Link from "next/link";
+import { MotionButton } from "@/components/MotionButton";
 
 export default function AboutPage() {
   return (
@@ -26,16 +27,7 @@ export default function AboutPage() {
 
       {/* resume centered */}
       <div className="mt-8 flex justify-center">
-        <Link
-          href={site.resumePath}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="inline-flex items-center rounded-full px-4 py-2 text-sm font-medium
-                     ring-1 ring-black/15 dark:ring-white/20
-                     hover:bg-black/5 dark:hover:bg-white/5 transition"
-        >
-          Download Resume
-        </Link>
+        <MotionButton href={site.resumePath}>View Resume</MotionButton>
       </div>
     </Section>
   );
